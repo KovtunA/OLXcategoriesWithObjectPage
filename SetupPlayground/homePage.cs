0e67c151af5b54d97c215488fe62dff12123eb0c
KovtunA <42818685+KovtunA@users.noreply.github.com>
2018-10-16 13:31:18 +0300

    7 tests + 2 failed

--- SetupPlayground/homePage.cs
+++ SetupPlayground/homePage.cs
@@ -42,6 +42,31 @@ namespace SetupPlayground
         [FindsBy(How = How.CssSelector, Using = "#bottom6 > div > a > strong")]
         public IWebElement showWork;
 
+        [FindsBy(How = How.CssSelector, Using = "#searchmain-container > div > div > div:nth-child(6) > div:nth-child(2) > div > a > span:nth-child(1)")]
+        public IWebElement animalCategory;
+
+        [FindsBy(How = How.CssSelector, Using = "#bottom35 > div > a > strong")]
+        public IWebElement showAnimal;
+
+
+        [FindsBy(How = How.CssSelector, Using = "#searchmain-container > div > div > div:nth-child(6) > div:nth-child(3) > div > a > span:nth-child(1)")]
+        public IWebElement houseCategory;
+
+        [FindsBy(How = How.CssSelector, Using = "#bottom899 > div > a > strong")]
+        public IWebElement showHouse;
+
+        [FindsBy(How = How.CssSelector, Using = "#searchmain-container > div > div > div:nth-child(6) > div:nth-child(4) > div > a > span:nth-child(1)")]
+        public IWebElement gadgetsCategory;
+
+        [FindsBy(How = How.CssSelector, Using = "#bottom37 > div > a > strong")]
+        public IWebElement showGadgets;
+
+        [FindsBy(How = How.CssSelector, Using = "#searchmain-container > div > div > div:nth-child(11) > div:nth-child(1) > div > a > span:nth-child(1)")]
+        public IWebElement BusinessCategory;
+
+        [FindsBy(How = How.CssSelector, Using = "#bottom7 > div > a > strong")]
+        public IWebElement showbusiness;
+
 
         public homePage(IWebDriver browser)
         {
@@ -86,12 +111,46 @@ namespace SetupPlayground
             browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
 
-        public void openWork()
+        public void openWorkPage()
         {
             workCategory.Click();
             browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             showWork.Click();
             browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
+
+        public void openAnimalPage()
+        {
+            animalCategory.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            showAnimal.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+        }
+
+        public void openHousePage()
+        {
+            houseCategory.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            showHouse.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+        }
+
+        public void openGadgetsPage()
+        {
+            gadgetsCategory.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            showGadgets.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+        public void openBusinessPage()
+        {
+            gadgetsCategory.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            showGadgets.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
     }
 }
