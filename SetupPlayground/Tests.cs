6fa5c0071d3b4e6fce50d252a1b70cb4dfccf16a
KovtunA <42818685+KovtunA@users.noreply.github.com>
2018-10-16 13:01:25 +0300

    4 tests

--- SetupPlayground/Tests.cs
+++ SetupPlayground/Tests.cs
@@ -28,15 +28,101 @@ namespace SetupPlayground
         }
 
         [Test]
-        public void RealestatePag()
-        {
-            homePage.openRealestatePage();
-        }
+        public void RealestatePage()
+        { homePage.openRealestatePage(); }
 
         [Test]
         public void carsPage()
+        { homePage.openCarsPage(); }
+
+        [Test]
+        public void kidsPage()
+        { homePage.openKidsPage();  }
+
+
+        [Test]
+        public void SparePartPage()
+        { homePage.openSparePartsPage(); }
+
+        [Test]
+        public void WorkPage()
+        { homePage.openWork(); }
+
+        [Test]
+        public void rabota()
         {
-            homePage.openCarsPage();
+            var nameOfCategoria = "#searchmain-container > div > div > div:nth-child(6) > div:nth-child(1) > div > a > span:nth-child(1)";
+            CategoriesOfMain(nameOfCategoria, "#bottom6 > div > a > strong");
+        }
+
+        [Test]
+        public void animal()
+        {
+            var nameOfCategoria = "#searchmain-container > div > div > div:nth-child(6) > div:nth-child(2) > div > a > span:nth-child(1)";
+            var buttonAll = "#bottom35 > div > a > strong";
+            CategoriesOfMain(nameOfCategoria, buttonAll);
+
+        }
+
+        [Test]
+        public void dom()
+        {
+            var nameOfCategoria = "#searchmain-container > div > div > div:nth-child(6) > div:nth-child(3) > div > a > span:nth-child(1)";
+            var buttonAll = "#bottom899 > div > a > strong";
+            CategoriesOfMain(nameOfCategoria, buttonAll);
+
+        }
+
+        [Test]
+        public void electron()
+        {
+            var nameOfCategoria = "#searchmain-container > div > div > div:nth-child(6) > div:nth-child(4) > div > a > span:nth-child(1)";
+            var buttonAll = "#bottom37 > div > a > strong";
+            CategoriesOfMain(nameOfCategoria, buttonAll);
+
+        }
+
+        [Test]
+        public void bussines()
+        {
+            var nameOfCategoria = "#searchmain-container > div > div > div:nth-child(11) > div:nth-child(1) > div > a > span:nth-child(1)";
+            CategoriesOfMain(nameOfCategoria, "#bottom7 > div > a > strong");
+        }
+
+        [Test]
+        public void moda()
+        {
+            var nameOfCategoria = "#searchmain-container > div > div > div:nth-child(11) > div:nth-child(2) > div > a > span:nth-child(1)";
+            CategoriesOfMain(nameOfCategoria, "#bottom891 > div > a > strong");
+        }
+
+        [Test]
+        public void hobbi()
+        {
+            var nameOfCategoria = "#searchmain-container > div > div > div:nth-child(11) > div:nth-child(3) > div > a > span:nth-child(1)";
+            CategoriesOfMain(nameOfCategoria, "#bottom903 > div > a > strong");
+        }
+
+        [Test]
+        public void darom()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Navigate().GoToUrl("https://www.olx.ua");
+            IWebElement categoria = browser.FindElement(By.CssSelector("#searchmain-container > div > div > div:nth-child(11) > div:nth-child(4) > div > a > span:nth-child(1)"));
+            categoria.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            browser.Close();
+        }
+
+        [Test]
+        public void obmen()
+        {
+            IWebDriver browser = new ChromeDriver();
+            browser.Navigate().GoToUrl("https://www.olx.ua");
+            IWebElement categoria = browser.FindElement(By.CssSelector("#searchmain-container > div > div > div:nth-child(16) > div.li.fleft > div > a > span:nth-child(1)"));
+            categoria.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            browser.Close();
         }
 
         [TearDown]
--- SetupPlayground/homePage.cs
+++ SetupPlayground/homePage.cs
@@ -9,6 +9,9 @@ namespace SetupPlayground
         IWebDriver browser;
         string homePageURL = "https://www.olx.ua";
 
+        [FindsBy(How = How.CssSelector, Using = "#headerLogo")]
+        public IWebElement logoButton;
+
         [FindsBy(How = How.CssSelector, Using = "#searchmain-container > div > div > div:nth-child(1) > div:nth-child(2) > div")]
         public IWebElement realestateCategory;
 
@@ -21,8 +24,24 @@ namespace SetupPlayground
         [FindsBy(How = How.CssSelector, Using = "#bottom1532 > div > a")]
         public IWebElement showCars;
 
-        [FindsBy(How = How.CssSelector, Using = "#headerLogo")]
-        public IWebElement logoButton;
+        [FindsBy(How = How.CssSelector, Using = "#searchmain-container > div > div > div:nth-child(1) > div:nth-child(1) > div > a")]
+        public IWebElement kidsCategory;
+
+        [FindsBy(How = How.CssSelector, Using = "#bottom36 > div > a")]
+        public IWebElement showkids;
+
+        [FindsBy(How = How.CssSelector, Using = "#searchmain-container > div > div > div:nth-child(1) > div:nth-child(4) > div > a > span:nth-child(1)")]
+        public IWebElement SparePartsCategory;
+
+        [FindsBy(How = How.CssSelector, Using = "#bottom3 > div > a > strong")]
+        public IWebElement showSpereParts;
+
+        [FindsBy(How = How.CssSelector, Using = "#searchmain-container > div > div > div:nth-child(6) > div:nth-child(1) > div > a > span:nth-child(1)")]
+        public IWebElement workCategory;
+
+        [FindsBy(How = How.CssSelector, Using = "#bottom6 > div > a > strong")]
+        public IWebElement showWork;
+
 
         public homePage(IWebDriver browser)
         {
@@ -48,6 +67,30 @@ namespace SetupPlayground
             carCategory.Click();
             browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             showCars.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        public void openKidsPage()
+        {
+            kidsCategory.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            showkids.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        public void openSparePartsPage()
+        {
+            SparePartsCategory.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            showSpereParts.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        public void openWork()
+        {
+            workCategory.Click();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            showWork.Click();
             browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
     }
